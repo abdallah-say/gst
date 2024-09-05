0057588b695d2756e454d75566ac434c7b38ef9f
SnakeEye <abdallah.s_ah@outlook.com>
2024-09-05 18:59:22 +0300

    table editing

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,16 +6,7 @@ export function Table({ rows, deleteRow, editRow, headers }) {
       <div className="table-wrapper">
         <table className="table">
           <thead>
-            {/* <tr
-              style={{ gridTemplateColumns: `repeat(${headers.length}, 1fr)` }}
-            >
-              {headers.map((header, index) => (
-                <th key={index}>{header}</th>
-              ))} */}
-            {/* </tr> */}
-            <tr
-              style={{ gridTemplateColumns: `repeat(${headers.length}, 1fr)` }}
-            >
+            <tr>
               {headers.map((key) => (
                 <th key={key}>{key}</th>
               ))}
@@ -23,15 +14,13 @@ export function Table({ rows, deleteRow, editRow, headers }) {
             </tr>
           </thead>
           <tbody>
-            {/* {rows.map((row, idx) => {
-              const statusText =
-                row.status.charAt(0).toUpperCase() + row.status.slice(1);
+            {rows.map((row, idx) => {
+              const statusText = row.status
+                ? row.status.charAt(0).toUpperCase() + row.status.slice(1)
+                : "N/A";
 
               return (
-                <tr
-                  key={idx}
-                  style={{ gridTemplateColumns: `repeat(${row.length}, 1fr)` }}
-                >
+                <tr key={idx}>
                   <td>{row.fname}</td>
                   <td>{row.email}</td>
                   <td>
@@ -54,29 +43,7 @@ export function Table({ rows, deleteRow, editRow, headers }) {
                   </td>
                 </tr>
               );
-            })} */}
-            {rows.map((element, index) => (
-              <tr
-                key={index}
-                style={{
-                  gridTemplateColumns: `repeat(${element.length}, 1fr)`,
-                }}
-              >
-                {headers.map((key) => (
-                  <td key={key}>
-                    {key === "Price"
-                      ? `${element[key]}$`
-                      : element[key] === null
-                      ? "null"
-                      : element[key]}
-                  </td>
-                ))}
-                <td id="products-action-Table">
-                  <button>Update</button>
-                  <button>Delete</button>
-                </td>
-              </tr>
-            ))}
+            })}
           </tbody>
         </table>
       </div>
